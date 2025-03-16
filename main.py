a2f5b003aafa282ee9507eb205c71ff1a2839b22
James Patrick Oentoro <79458424+JamesPatrickOentoro@users.noreply.github.com>
2025-03-16 23:01:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from detect import *
 from translate import *
 from place_text import *
+from handle_table import *
 import vertexai
 from vertexai.preview.generative_models import GenerativeModel, Part, SafetySetting
 import os
@@ -9,7 +10,7 @@ import tempfile
 import gradio as gr
 
     
-def main(save_pdf, pdf_file,chosen_model):
+def main(save_pdf, pdf_file,chosen_model,translate_table='False'):
     # save_pdf = save_pdf + ".pdf"
     vertexai.init(project="dla-presales-team-sandbox", location="us-central1")
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "presales_cred.json"
@@ -35,8 +36,13 @@ def main(save_pdf, pdf_file,chosen_model):
             "temperature": 0.8,
             "top_p": 0.95,
         }
-    text_data, shape = bounding_box(pdf_file, model_ocr)
+    # if translate_table == 'True':
+    #     file = replace_table_with_image(pdf_file, 'temp.pdf')
+    #     pdf_file = 'temp.pdf'
+    text_data, shape = bounding_box(pdf_file, model_ocr, translate_table)
     print("SUCCESS OCR")
+    # print(text_data)
+
     translated = translate(model,generation_config,text_data)
     print("SUCCESS TRANSLATED")
     file = annotate_pdf_multiple(pdf_file, save_pdf, translated, shape)
@@ -51,11 +57,13 @@ with gr.Blocks() as demo:
         with gr.Column():
             pdf_file = gr.UploadButton(file_types=[".pdf"])
             save_pdf = gr.Textbox(label="Result Pdf File Name")
+            trans_table=gr.Dropdown(["True", "False"], label="Translate table")
             chosen_model = gr.Dropdown(choices=["RCNN X101","RCNN R50", "Fast RCNN R50", "P RCNN R50"], label="ocr_model", info="Choose OCR Model")
         with gr.Column():
             translate_btn = gr.Button(value="Translate Document")
-            translate_btn.click(fn=main, inputs = [save_pdf,pdf_file,chosen_model], outputs=gr.File(label="Download Annotated PDF", file_types=[".pdf"]))
-            
+            translate_btn.click(fn=main, inputs = [save_pdf,pdf_file,chosen_model,trans_table], outputs=gr.File(label="Download Annotated PDF", file_types=[".pdf"]))
 
-demo.launch()
+if __name__ == "__main__":      
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(server_port=port, server_name="0.0.0.0")
     
